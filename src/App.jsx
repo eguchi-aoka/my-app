e191947a3edd6e3a97bfd2c016afa8f4155ab504
Eguchi Aoka <aoka0316@icloud.com>
2026-02-08 16:30:22 +0900

    不要なページを削除

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
-import Home from './page/home'
-import About from './page/about'
-import Contact from './page/contact'
 import Weather from './page/weather'
 import './App.css'
 
@@ -10,16 +7,10 @@ function App() {
    <BrowserRouter>
    <nav>
     <ul>
-     <li><Link to="/">Home</Link></li>
-     <li><Link to="/about">About</Link></li>
-     <li><Link to="/contact">Contact</Link></li>
      <li><Link to="/weather">Weather</Link></li>
      </ul>
        <hr />
      <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/contact' element={<Contact />} />
       <Route path='/weather' element={<Weather />} />
      </Routes>
    </nav>
--- src/page/contact.jsx
+++
@@ -1,9 +0,0 @@
-function Contact () {
-    return (
-        <>
-        <h1>お問い合わせ</h1>
-        </>
-        )
-}
-
-export default Contact;
