44bd4c8d42be25d7f11b87f89a5c7e392c6d91d9
Eguchi Aoka <aoka0316@icloud.com>
2026-02-08 16:34:29 +0900

    余計なページを処分し、weatherをメインに配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 import Weather from './page/weather'
 import './App.css'
 
 function App() {
-   return (
-   <BrowserRouter>
-   <nav>
-    <ul>
-     <li><Link to="/weather">Weather</Link></li>
-     </ul>
-       <hr />
-     <Routes>
-      <Route path='/weather' element={<Weather />} />
-     </Routes>
-   </nav>
-</BrowserRouter>
-   )
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  )
 }
 
 export default App
